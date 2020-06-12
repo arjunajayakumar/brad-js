3e1fa6267eae8bac07b9aed864c5069dadaaedb7
Arjun <arjune64@gmail.com>
2020-06-12 11:51:04 +0530

    js fundementals

---
+++ 02_js_fundementals/07_variables.js
@@ -0,0 +1,21 @@
+let greeting = 'hello';
+greeting='john'
+console.log(greeting);
+
+const name = 'sara';
+console.log(name);
+
+const person = {
+  name:'john',
+  age:'35'
+}
+
+person.name='sara';
+person.age='45'
+
+console.log(person);
+
+const number = [1,2,3,5,7];
+number.push(8);
+console.log(number);
+
---
+++ 02_js_fundementals/08_datatypes.js
@@ -0,0 +1,33 @@
+// PRIMITIVE
+// String
+const name = 'John doe';
+console.log(typeof name);
+// Number 
+const number = 12;
+console.log(typeof number);
+// boolean
+const hasKids = true;
+console.log(typeof hasKids);
+// Null
+const car = null;
+console.log(typeof car);
+// Undefined
+let test;
+console.log(typeof test);
+// symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+// REFERENCE TYPES - OBJECTS
+// Array
+const hobbies=['movies','music']
+console.log(typeof hobbies);
+// Object literal
+const address = {
+  city:'newyork',
+  state:'MA'
+}
+console.log(typeof address)
+const today = new Date();
+console.log(today);
+console.log(typeof today);

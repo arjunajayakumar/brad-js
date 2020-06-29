7aa447193b9803055feec39dd3ef7ed7220bbb90
Arjun <arjune64@gmail.com>
2020-06-29 22:00:31 +0530

    Tutorial 78 to Tutorial 81

---
+++ apiProjects/079_weatherJS[Part_2]-Intro UI.js
@@ -0,0 +1,16 @@
+// Init weather Object
+const weather = new Weather('Kochi', 'kerala');
+
+// Get weather on DOM Load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+
+// weather.changeLocation('london', 'uk');
+
+function getWeather() {
+    weather.getWeather()
+    .then(results => {
+        console.log(results);
+    })
+    .catch(err => console.log(err)) 
+}
---
+++ apiProjects/081_weather.js
@@ -0,0 +1,24 @@
+class Weather {
+    constructor(city, state) {
+        this.apiKey = '44305646923b1d8a7e8017e26347dad7';
+        this.city = city;
+        this.state = state;
+    }
+
+    // Fetch weather from api
+    async getWeather() {
+    
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric, ${this.state}&appid=${this.apiKey}`);
+       
+        const responseData = await response.json();
+        return responseData;
+
+    }
+
+    // Change Location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
+

d5c17dc9dc727254f2174084a1666406973862a5
Arjun <arjune64@gmail.com>
2020-07-19 01:51:04 +0530

    Implemented row navigation, coloumn navigation and display text on tap

--- t43_rotatingKeyboard/t43_rotatingKeyboard.js
+++ t43_rotatingKeyboard/t43_rotatingKeyboard.js
@@ -1,66 +1,62 @@
-const tapBtn = document.getElementById('input');
-const output = document.getElementById('output');
-const classList = document.querySelectorAll('keys');
-const row1 = document.querySelector('.row1');
-const row2 = document.querySelector('.row2');
-const row3 = document.querySelector('.row3');
-const row4 = document.querySelector('.row4');
-const row5 = document.querySelector('.row5');
-const row6 = document.querySelector('.row6');
-
+const tapBtn = document.getElementById("input");
+const output = document.getElementById("output");
+const classList = document.querySelectorAll("keys");
+const row1 = document.querySelector(".row1");
+const row2 = document.querySelector(".row2");
+const row3 = document.querySelector(".row3");
+const row4 = document.querySelector(".row4");
+const row5 = document.querySelector(".row5");
+const row6 = document.querySelector(".row6");
 
 //Event Listeners
-// tapBtn.addEventListener('click', navigation);
+tapBtn.addEventListener("click", navigation);
 
 let rows = [row1, row2, row3, row4, row5, row6];
-let index = 0;
+let currentIndex = 0;
+let previousIndex = 5;
+let currentIndexCol = 0;
+let previousIndexCol = 11;
+let state = "initial";
 
-// let runner;
-// let state  = 'rowNav';
-
-// function navigation() {
-//   if(state === 'rowNav') {
-//       state = 'coloumn';
-//       rowNav();
-//   } else if(state === 'coloumn') {
-//       state = 'rowNav';
-//      clearInterval(time)
-//       selectColoumn()
-//   } else {
-//       displayText();
-//   }
-
-// }
-
-
-let interval = 2000;
-let clear;
-
-function rowNav() {
-  time = setInterval(() => {
-    let currentVble, nextVble;
-    if(rows[index++ % rows.length].classList.contains('.rowcolor')) {
-      console.log('no');
-
-      rows[index++ % rows.length].classList.remove('rowcolor');
-    } else {
-      rows[index++ % rows.length].classList.add('rowcolor');
-      console.log('yes');
-    }
-  }, interval);  
+// function to handle clicks
+function navigation() {
+  if (state === "initial") {
+    state = "rowNav";
+    rowNav();
+  } else if (state === "rowNav") {
+    state = "colNav";
+    clearInterval(time);
+    selectColoumn();
+  } else {
+    displayText();
+  }
 }
 
-// rowNav()
+// Row Navigation
+let interval = 1000;
 
+function rowNav() {
+  time = window.setInterval(() => {
+    rows[currentIndex].classList.add("rowcolor");
+    rows[previousIndex].classList.remove("rowcolor");
+    previousIndex = currentIndex;
+    currentIndex = (currentIndex + 1) % 6;
+  }, interval);
+}
 
+// Coloumn Navigation
+function selectColoumn() {
+  time = window.setInterval(() => {
+    rows[previousIndex].children[currentIndexCol].classList.add("single");
+    rows[previousIndex].children[previousIndexCol].classList.remove("single");
+    previousIndexCol = currentIndexCol;
+    currentIndexCol = (currentIndexCol + 1) % 12;
+  }, interval);
+}
 
-
-// function selectColoumn() {
-//   console.log('not written')
-// }
-
-
-// function displayText() {
-//     console.log('not written')
-// }
-   
+// Display text
+function displayText() {
+  rows[previousIndex].children[previousIndexCol].classList.contains("single");
+  output.value += rows[previousIndex].children[previousIndexCol].textContent;
+  console.log(previousIndexCol);
+}

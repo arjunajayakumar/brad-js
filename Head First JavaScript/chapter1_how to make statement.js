8b15d651baa082f8844f2c0192c8c58bdb4cb423
Arjun <arjune64@gmail.com>
2020-07-30 11:38:23 +0530

    chapter 1: page 1 to page 11

---
+++ Head First JavaScript/chapter1_how to make statement.js
@@ -0,0 +1,8 @@
+let age = 25;
+let name = "owen";
+
+if(age > 14) {
+    alert("Sorry this page is for kids only!");
+} else {
+    alert("welcome " + name + "!");
+}
---
+++ Head First JavaScript/chapter_1variales and values.js
@@ -0,0 +1,6 @@
+let winners = 2;
+let name = "Duke";
+let isEligible = false;
+console.log(isEligible);
+
+let losers;

cc473e5cead05947873b1491fda442a4444dff3b
Arjun <arjune64@gmail.com>
2020-08-02 22:37:49 +0530

    chapter 5 initial commit

---
+++ Head First JavaScript/chapter5_aTripToObjectville.js
@@ -0,0 +1,13 @@
+// Car Object
+
+let car = {
+    make:"chevy",
+    model:"Bel Air",
+    year:"1957",
+    color:"red",
+    passengers:2,
+    convertible:false,
+    mileage:1021
+}
+
+console.log(car)

0de57f04f47378b1e7f4152dc13b2f7fe61c5f22
Arjun <arjune64@gmail.com>
2020-08-05 23:01:12 +0530

    chapter 7 initial commit

---
+++ Head First JavaScript/chapter7_seriousTypes.js
@@ -0,0 +1,15 @@
+
+// Dealing with NaN
+// let myNum = 0;
+
+// if(isNaN(myNum)) {
+//     myNum = 0;
+//     console.log(myNum)
+// } else {
+//     console.log(myNum + " is a number")
+// }
+
+// it gets even wirder
+// let test1 = 0/0;
+// console.log(test1);
+// console.log('Type:', typeof test1);

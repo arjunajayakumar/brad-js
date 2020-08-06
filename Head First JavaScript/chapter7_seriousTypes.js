e68428732adb39e1b2837915bb6f40eb29c1b206
Arjun <arjune64@gmail.com>
2020-08-06 23:13:11 +0530

    chapter 7 289

--- Head First JavaScript/chapter7_seriousTypes.js
+++ Head First JavaScript/chapter7_seriousTypes.js
@@ -13,3 +13,37 @@
 // let test1 = 0/0;
 // console.log(test1);
 // console.log('Type:', typeof test1);
+
+if (99 === "99") {
+    console.log("A number equals to a string");
+} else {
+    console.group("No way a number equals a string");
+}
+
+
+function findCarInLot(car) {
+for (var i = 0; i < lot.length; i++) {
+if (car === lot[i]) {
+return i;
+}
+}
+return -1;
+}
+var chevy = {
+make: "Chevy",
+model: "Bel Air"
+};
+var taxi = {
+make: "Webville Motors",
+model: "Taxi"
+};
+var fiat1 = {
+make: "Fiat",
+model: "500"
+};
+var fiat2 = {
+make: "Fiat",
+model: "500"
+};
+
+var lot = [chevy, taxi, fiat1, fiat2];

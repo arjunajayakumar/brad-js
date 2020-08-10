cf649a8bb501f8499eeca3b6ea21e77f6a334c3f
arjunajayakumar <arjune64@gmail.com>
2020-08-10 23:27:02 +0530

    Chapter 9 Page 398

--- Head First JavaScript/chapter9_HandlingEvents.js
+++ Head First JavaScript/chapter9_HandlingEvents.js
@@ -5,13 +5,24 @@
 
 
 window.onload = init;
+// function init() {
+//     let image = document.getElementById("zero");
+//     image.onclick = showAnswer;
+
+// }
+
+// Assigning the click handler to all images on the page
 function init() {
-    let image = document.getElementById("zero");
-    image.onclick = showAnswer;
-    
+    let images = document.getElementsByTagName("img");
+    console.log(images)
+    for (let i = 0; i < images.length; i++) {
+        images[i].onclick = showAnswer;
+    }
+
+
 }
 
-function showAnswer(){
+function showAnswer() {
     let image = document.getElementById("zero");
     image.src = "/img/zero.jpg";
 }

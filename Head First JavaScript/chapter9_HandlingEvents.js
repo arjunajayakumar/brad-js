b526ed6ac3c916491d0d996ecbea0e1de6eeef79
arjunajayakumar <arjune64@gmail.com>
2020-08-11 11:45:55 +0530

    Chapter 9 Page 414

--- Head First JavaScript/chapter9_HandlingEvents.js
+++ Head First JavaScript/chapter9_HandlingEvents.js
@@ -35,14 +35,65 @@ window.onload = init;
 // }
 
 // The mouse move event
-function init() {
-    let map = document.getElementById("map");
-    map.onmousemove = showCords;
-}
+// function init() {
+//     let map = document.getElementById("map");
+//     map.onmousemove = showCords;
+// }
 
-function showCords(eventObj){
-    let map = document.getElementById("coords");
-    let x = eventObj.screenX;
-    let y = eventObj.screenY;
-    map.innerHTML = "Map coordinates: " + x + ", " + y;
-}
+// function showCords(eventObj){
+//     let map = document.getElementById("coords");
+//     let x = eventObj.screenX;
+//     let y = eventObj.screenY;
+//     map.innerHTML = "Map coordinates: " + x + ", " + y;
+// }
+
+// SetTimeout
+// function timerHandler(){
+//     alert("Hey what are you doing just sitting there staring at ablank screen?");
+// }
+
+// setTimeout(timerHandler, 5000);
+
+// SetTimeInterval
+// let tick = true;
+// function ticker(){
+//     if(tick) {
+//         console.log("tick");
+//         tick = false;
+//     } else {
+//         console.log('tock');
+//         tick = true;
+//     }
+// }
+
+
+// setInterval(ticker, 1000);
+
+// Image blur using SetTimeout
+// function init() {
+//     let images = document.getElementsByTagName("img");
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].onclick = showAnswer;
+//     }
+
+
+// }
+
+// function showAnswer(eventObj){
+//     let image = eventObj.target;
+//     let name = image.id;
+//     name = name + ".jpg";
+//     image.src = "/img/" + name;
+
+//     setTimeout(reblur, 2000, image);
+// }
+
+// function reblur(image){
+//     let name = image.id;
+//     name = name + "blur.jpg";
+//     image.src= "/img/" + name;
+// }
+
+
+11/08/2020
+

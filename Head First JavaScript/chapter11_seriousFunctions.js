27570a91ad904169f88bd2e9a5a098c70ebc45d7
arjunajayakumar <arjune64@gmail.com>
2020-08-13 00:28:01 +0530

    Chapter 11 page 482

---
+++ Head First JavaScript/chapter11_seriousFunctions.js
@@ -0,0 +1,8 @@
+// How to use an anonymous funcion
+// window.onload = function() {
+//     console.log("Yeah, that Page loaded!")
+// }
+
+// We need to walk about your verbosity, again
+setTimeout(function() {console.log("Time to take the cookies out of the oven");
+},5000);

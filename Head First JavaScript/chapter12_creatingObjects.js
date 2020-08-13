d932f97af1c14061045c19cb7eac6d85057a3a42
arjunajayakumar <arjune64@gmail.com>
2020-08-14 00:05:18 +0530

    Chapter 12 Page 535

--- Head First JavaScript/chapter12_creatingObjects.js
+++ Head First JavaScript/chapter12_creatingObjects.js
@@ -11,7 +11,7 @@
 //     stop:function() {this.started = false},
 //     drive:function() {
 //         console.log("woom woom")
-        
+
 //     }
 
 // };
@@ -38,23 +38,78 @@
 // console.log(fluffy);
 
 // Exercise
-function Dog(name, breed, weight) {
-    this.name = name;
-    this.breed = breed;
-    this.weight = weight;
+// function Dog(name, breed, weight) {
+//     this.name = name;
+//     this.breed = breed;
+//     this.weight = weight;
+//     }
+
+//     let fido = new Dog("Fido", "Mixed", 38);
+//     let fluffy = new Dog("Fluffy", "Poodle", 30);
+//     let spot = new Dog("Spot", "Chihuahua", 10);
+//     let dogs = [fido, fluffy, spot];
+
+//     for(let i = 0; i < dogs.length; i++){
+//         let size = "small";
+//         if(dogs[i].weight > 10){
+//             size = "large";
+//         }
+
+//         console.log("Dog: " + dogs[i].name + " is a " + size + " " + dogs[i].breed);
+//     }
+
+// Putting Methods into constructors as well
+// function Dog(name, breed, weight) {
+//     this.name = name;
+//     this.breed = breed;
+//     this.weight = weight;
+//     this.bark = function () {
+//         if (this.weight > 25) {
+//             console.log(this.name + " says Woof!");
+//         } else {
+//             console.log(this.name + " says Yip!");
+//         }
+//     }
+// }
+
+// let fido = new Dog("Fido", "Mixed", 38);
+// let fluffy = new Dog("Fluffy", "Poodle", 30);
+// let spot = new Dog("Spot", "Chihuahua", 10);
+// let dogs = [fido, fluffy, spot];
+// for (let i = 0; i < dogs.length; i++) {
+//     dogs[i].bark();
+// }
+
+// Exercise
+function Coffee(roast, ounces) {
+    this.roast = roast;
+    this.ounces = ounces;
+    this.getSize = function () {
+        if (this.ounces === 8) {
+            return "8oz is a small";
+        } else if (this.ounces === 12) {
+            return "12oz is a medium";
+        } else if (this.ounces === 16) {
+            return "16oz is a large";
+        }
     }
 
-    let fido = new Dog("Fido", "Mixed", 38);
-    let fluffy = new Dog("Fluffy", "Poodle", 30);
-    let spot = new Dog("Spot", "Chihuahua", 10);
-    let dogs = [fido, fluffy, spot];
-
-    for(let i = 0; i < dogs.length; i++){
-        let size = "small";
-        if(dogs[i].weight > 10){
-            size = "large";
+    this.toString = function () {
+        if (this.ounces === 8) {
+            return "you have ordered a small " + this.roast + " coffee";
+        } else if (this.ounces === 12) {
+            return "you have ordered a medium " + this.roast + " coffee";
+        } else if (this.ounces === 16) {
+            return "you have ordered a large " + this.roast + " coffee";
         }
 
-        console.log("Dog: " + dogs[i].name + " is a " + size + " " + dogs[i].breed);
     }
-    
+}
+
+let houseBlend = new Coffee("House Blend", 12);
+console.log(houseBlend.getSize());
+
+let darkRoast = new Coffee("Dark Roast", 16);
+console.log(darkRoast.toString());
+
+

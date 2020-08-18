a0789a822470a363fa0d24f03b09d37aae1fa7ca
arjunajayakumar <arjune64@gmail.com>
2020-08-18 13:09:51 +0530

    Chapter 13 completed

--- Head First JavaScript/chapter13_extraStrengthObjects.js
+++ Head First JavaScript/chapter13_extraStrengthObjects.js
@@ -27,69 +27,69 @@ let spot = new Dog("Spot", "Chihuahua", 10);
 
 console.log(spot);
 
-// fido.bark();
-// fido.run();
-// fido.wag();
+fido.bark();
+fido.run();
+fido.wag();
 
-// fluffy.bark();
-// fluffy.run();
-// fluffy.wag();
+fluffy.bark();
+fluffy.run();
+fluffy.wag();
 
-// spot.bark();
-// spot.run();
-// spot.wag();
+spot.bark();
+spot.run();
+spot.wag();
 
-// spot.bark = function() {
-//     console.log(this.name + " says WOOF!");
-// };
+spot.bark = function() {
+    console.log(this.name + " says WOOF!");
+};
 
-// fido.bark();
-// fido.run();
-// fido.wag();
+fido.bark();
+fido.run();
+fido.wag();
 
-// fluffy.bark();
-// fluffy.run();
-// fluffy.wag();
+fluffy.bark();
+fluffy.run();
+fluffy.wag();
 
-// spot.bark();
-// spot.run();
-// spot.wag();
+spot.bark();
+spot.run();
+spot.wag();
 
-// function Robot(name, year, owner) {
-//     this.name = name;
-//     this.year = year;
-//     this.owner = owner;
-// }
+function Robot(name, year, owner) {
+    this.name = name;
+    this.year = year;
+    this.owner = owner;
+}
 
-// Robot.prototype.maker = "Dallas";
-// Robot.prototype.speak = function() {
-//     console.log("warning warning");
-// }
-// Robot.prototype.makeCoffee = function() {
-//     console.log("Making Coffee");
-// }
-// Robot.prototype.blinkLights = function() {
-//     console.log("Blink blink")
-// }
+Robot.prototype.maker = "Dallas";
+Robot.prototype.speak = function() {
+    console.log("warning warning");
+}
+Robot.prototype.makeCoffee = function() {
+    console.log("Making Coffee");
+}
+Robot.prototype.blinkLights = function() {
+    console.log("Blink blink")
+}
 
-// let robby = new Robot("dodgy", 2005, "robert");
-// let rosie = new Robot("Ken", 1989, "Tommy");
+let robby = new Robot("dodgy", 2005, "robert");
+let rosie = new Robot("Ken", 1989, "Tommy");
 
-// robby.onOffSwitch = true;
-// robby.makeCoffee = function() {
-//     console.log("Fetching a coffee from starBucks");
-// };
+robby.onOffSwitch = true;
+robby.makeCoffee = function() {
+    console.log("Fetching a coffee from starBucks");
+};
 
-// rosie.cleanHouse = function() {
-//     console.log("Cleaning! Spic and span soon");
-// }
+rosie.cleanHouse = function() {
+    console.log("Cleaning! Spic and span soon");
+}
 
-// console.log(robby.name + " was made by " + robby.maker + " in " + robby.year + " and owned by " + robby.owner);
-// robby.makeCoffee();
-// robby.blinkLights();
+console.log(robby.name + " was made by " + robby.maker + " in " + robby.year + " and owned by " + robby.owner);
+robby.makeCoffee();
+robby.blinkLights();
 
-// console.log(rosie.name + " was made by " + rosie.maker + " in "  + rosie.year + " and owned by " + rosie.owner);
-// rosie.cleanHouse();
+console.log(rosie.name + " was made by " + rosie.maker + " in "  + rosie.year + " and owned by " + rosie.owner);
+rosie.cleanHouse();
 
 let barnaby = new Dog("Barnaby", "Basset Hound", 55);
 // let spot = new Dog("spot", "himansu",38);
@@ -100,44 +100,44 @@ Dog.prototype.sit = function() {
 barnaby.sit();
 
 
-// Exercise
+Exercise
 
-// function Game() {
-//     this.level = 0;
-// }
+function Game() {
+    this.level = 0;
+}
 
-// Game.prototype.play = function () {
-//     this.level++;
-//     console.log("Welcome to level " + this.level);
-//     this.unlock();
-// }
+Game.prototype.play = function () {
+    this.level++;
+    console.log("Welcome to level " + this.level);
+    this.unlock();
+}
 
-// Game.prototype.unlock = function () {
-//     if (this.level === 42) {
-//         Robot.prototype.deployLaser = function () {
-//             console.log(this.name + " is blasting you with the laser beam.....");
+Game.prototype.unlock = function () {
+    if (this.level === 42) {
+        Robot.prototype.deployLaser = function () {
+            console.log(this.name + " is blasting you with the laser beam.....");
 
-//         }
+        }
 
-//     }
-// }
+    }
+}
 
-// function Robot(name, year, owner) {
-//     this.name = name;
-//     this.year = year;
-//     this.owner = owner;
-// }
+function Robot(name, year, owner) {
+    this.name = name;
+    this.year = year;
+    this.owner = owner;
+}
 
-// let game = new Game();
-// let robby = new Robot("Robby", 1956, "Dr. Morbius");
-// let rosie = new Robot("Rosie", 1962, "George Jetson");
+let game = new Game();
+let robby = new Robot("Robby", 1956, "Dr. Morbius");
+let rosie = new Robot("Rosie", 1962, "George Jetson");
 
-// while (game.level < 42) {
-//     game.play();
-// }
+while (game.level < 42) {
+    game.play();
+}
 
-// robby.deployLaser();
-// rosie.deployLaser();
+robby.deployLaser();
+rosie.deployLaser();
 
 // A more interesting way implementataion of the sit method
 Dog.prototype.sitting = false;
@@ -150,10 +150,52 @@ Dog.prototype.sit = function() {
     }
 };
 
-// barnaby.sit();
-// barnaby.sit();
+barnaby.sit();
+barnaby.sit();
 
-// spot.sit();
-// spot.sit();
+spot.sit();
+spot.sit();
+
+console.log(spot.hasOwnProperty("species"));
+console.log(fido.hasOwnProperty("species"));
+
+console.log(spot.hasOwnProperty("sitting"));
+spot.sitting = true;
+console.log(spot.hasOwnProperty("sitting"));
 
 
+let aDog = new Dog();
+
+function showDog(name, breed, weight, handler) {
+    this.name = name;
+    this.breed = breed;
+    this.weeight = weight;
+    this.handler = handler;
+}
+
+showDog.prototype = new Dog();
+showDog.prototype.constructor = showDog;
+
+showDog.prototype.leauge = "Webville";
+showDog.prototype.stack = function() {
+    console.log("Stack");
+}
+
+showDog.prototype.bait = function() {
+    console.log("Bait");
+}
+
+showDog.prototype.gait = function(kind) {
+    console.log(kind + "ing");
+};
+
+showDog.prototype.groom = function() {
+    console.log("Groom");
+}
+
+let scotty = new showDog("Scotty", "Scotttish", 15, "Cookie");
+
+scotty.stack();
+scotty.bark();
+console.log(scotty.league);
+console.log(scotty.species);
